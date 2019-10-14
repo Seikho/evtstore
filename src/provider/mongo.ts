97dcdc75541c963c66e52509a500209f4dd8d46d
Carl Winkler <carl@winkler.id.au>
2019-10-14 11:27:10 +0800

    Add first pass of mongo index creation

--- src/provider/mongo.ts
+++ src/provider/mongo.ts
@@ -1,5 +1,6 @@
 import { Collection, Timestamp } from 'mongodb'
 import { UserEvt, StoredEvt, Provider } from '../types'
+import { VersionError } from './error'
 
 export type Bookmark = {
   bookmark: string
@@ -27,15 +28,33 @@ export function createProvider<E extends UserEvt>(
       const existing = await events.findOne({
         stream,
         version,
-        'event.aggregateId': event.aggregateId,
+        'event.aggregateId': event.aggregateId
       })
-      if (existing) throw new Error('Version conflict error')
+      if (existing) throw new VersionError()
       const timestamp = new Date(Date.now())
       const position = new Timestamp(0, 0)
 
       await events.insertOne({ stream, position, version, timestamp, event })
-    },
+    }
   }
+}
+
+export async function migrate(events: Collection<StoredEvt<any>>, bookmarks: Collection<Bookmark>) {
+  await bookmarks.createIndex({ bookmark: 1 }, { name: 'bookmark-index', unique: true })
+  await events.createIndex(
+    { stream: 1, position: 1 },
+    { name: 'stream-position-index', unique: true }
+  )
+
+  await events.createIndex(
+    { stream: 1, position: 1, 'event.type': 1 },
+    { name: 'stream-position-type-index', unique: true }
+  )
+
+  await events.createIndex(
+    { stream: 1, 'event.aggregateId': 1, version: 1 },
+    { name: 'stream-id-version-index', unique: true }
+  )
 }
 
 async function getPos(bm: string, coll: Collection<Bookmark>) {
@@ -43,9 +62,5 @@ async function getPos(bm: string, coll: Collection<Bookmark>) {
 }
 
 async function setPos(bm: string, pos: Timestamp, coll: Collection<Bookmark>) {
-  await coll.updateOne(
-    { bookmark: bm },
-    { $set: { position: pos } },
-    { upsert: true }
-  )
+  await coll.updateOne({ bookmark: bm }, { $set: { position: pos } }, { upsert: true })
 }
