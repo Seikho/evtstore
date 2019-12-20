ce0f0c22234e0c9c4a3822772706a7cd7581fd16
Carl Winkler <carl@winkler.id.au>
2019-12-20 17:24:04 +0800

    Ensure timestamp in meta is a date

--- src/common.ts
+++ src/common.ts
@@ -5,7 +5,7 @@ export function toMeta(ev: StoreEvent<any>): EventMeta {
     aggregateId: ev.aggregateId,
     position: ev.position,
     stream: ev.stream,
-    timestamp: ev.timestamp,
+    timestamp: new Date(ev.timestamp),
     version: ev.version,
   }
 }
