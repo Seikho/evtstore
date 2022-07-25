777e31e020c9dc09c1d9b87c4f754333c9187451
Carl Winkler <carl@winkler.id.au>
2022-07-25 15:35:16 +0800

    Export all types at top level

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-export { createDomainV1 } from './src/domain'
+export { createDomainV1, HandlerOptsV1 } from './src/domain'
 export { createHandler } from './src/create-handler'
 export { createBookmark } from './src/create-bookmark'
-export { createDomainV2, createDomain } from './src/domain-v2'
+export { createDomainV2, createDomain, HandlerOptions } from './src/domain-v2'
 export { createAggregate, createProvidedAggregate } from './src/create-aggregate'
 export { createCommands } from './src/create-command'
 export {
@@ -14,6 +14,22 @@ export {
   Handler,
   CmdBody,
   EventMeta,
+  StoreEvent,
+  StreamsHandler,
+  Aggregate,
+  BaseAggregate,
+  DomainHandlerOpts,
+  DomainOptions,
+  AggregateStore,
+  ErrorCallback,
+  HandlerBody,
+  HandlerBookmark,
+  HandlerHooks,
+  ProvidedAggregate,
+  StorableAggregate,
+  ProviderBookmark,
+  ExecutableAggregate,
+  Ext,
 } from './src/types'
 export { VersionError, CommandError } from './provider/error'
 export { MemoryBookmark } from './src/common'
